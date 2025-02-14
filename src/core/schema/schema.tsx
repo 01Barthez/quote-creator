e2005ad7af5485d0c05c1fcde84ff97fae917cc1
01Barthez <kenwoubarthez@gmail.com>
2025-02-14 10:55:58 +0100

    fix: Add form schema validation

--- src/core/schema/schema.tsx
+++ src/core/schema/schema.tsx
@@ -13,8 +13,30 @@ export const formSchema = z.object({
     })
 })
 
-export const formSchemaEmail = z.object({
-    email: z.string().email({
-        message: "Should be a correct email !"
-    }),
-})
+export const projectSchema = z.object({
+    name:
+        z.string({
+            required_error: "Name of projet is required !",
+            invalid_type_error: "The name format is not valid !"
+        })
+            .trim()
+            .min(3, {
+                message: "Name should be more than 3 characters !",
+            })
+            .max(150, {
+                message: "Name should be less than 150 characters !",
+            }),
+
+    description:
+        z.string({
+            required_error: "Description is required !",
+            invalid_type_error: "Description format is not valid !"
+        })
+            .trim()
+            .min(3, {
+                message: "Description should be more than 3 characters !",
+            })
+            .max(150, {
+                message: "Description should be less than 150 characters !",
+            }),
+});

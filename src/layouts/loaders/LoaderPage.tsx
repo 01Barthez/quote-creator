6f27d999ff4ca000f9f87e64e28bf88e2e040790
01Barthez <kenwoubarthez@gmail.com>
2025-02-14 14:28:16 +0100

    fix: Update loader Compoment

--- src/layouts/loaders/LoaderPage.tsx
+++ src/layouts/loaders/LoaderPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoaderPage: React.FC = () => {
   return (
-    <section className="absolute z-20 top-0 left-0 h-screen w-screen flex items-center justify-center">
+    <section className="absolute z-20 top-0 left-0 h-screen w-screen flex items-center justify-center bg-background">
       <div className="loader"></div>
     </section>
   )

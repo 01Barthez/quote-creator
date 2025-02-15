72addda78261ad9ddd836471c91135f550507a46
01Barthez <kenwoubarthez@gmail.com>
2025-02-15 23:11:23 +0100

    refactor: Update CTA compoment

--- src/components/custom/utils/CTA.tsx
+++ src/components/custom/utils/CTA.tsx
@@ -5,13 +5,13 @@ import { Button } from '@/components/ui/button';
 import { useNavigate } from "react-router";
 import type { ICTA } from '@/interface/interface';
 
-const CTA: React.FC<ICTA> = ({name="Create Projet", url}) => {
+const CTA: React.FC<ICTA> = ({ name = "Create Projet", url }) => {
     let navigate = useNavigate();
 
     return (
         <Button
             variant={'outline'}
-                className='w-fit border-primary/80 py-5 px-4 md:px-6 backdrop-blur-md bg-primary hover:bg-primary/85 text-primary-foreground hover:text-primary-foreground font-semibold'
+            className='w-fit border-primary/80 py-5 px-4 md:px-6 backdrop-blur-md bg-primary hover:bg-primary/85 text-primary-foreground hover:text-primary-foreground font-semibold'
             onClick={() => { navigate(`${url}`) }}
         >
             {name}

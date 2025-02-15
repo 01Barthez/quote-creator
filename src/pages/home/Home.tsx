c2518642d1d028a81327f7f7f39f89b45c7c671c
01Barthez <kenwoubarthez@gmail.com>
2025-02-15 23:12:36 +0100

    fix: Update Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import CTA from '@/components/custom/utils/CTA'
 import SEO from '@/components/custom/utils/SEO'
 import React from 'react'
+import { motion } from 'motion/react';
 
 const Home: React.FC = () => {
     return (
@@ -12,17 +13,36 @@ const Home: React.FC = () => {
             />
 
             {/* App Comoments */}
-            <div className='container py-10 md:py-24 lg:py-36 flex items-center justify-center flex-col gap-4 md:gap-6 lg:gap-10'>
-                <h1 className='text-xl md:text-2xl lg:text-4xl font-bold text-center'>
+            <section className='container py-10 md:py-16 lg:py-20 flex items-center justify-center flex-col gap-4 md:gap-6 lg:gap-10'>
+                {/* Title */}
+                <motion.h1
+                    className='max-w-[100%] md:max-w-[80%] lg:max-w-[70%] text-xl md:text-2xl lg:text-4xl font-bold text-center'
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.4, ease: "easeOut" }}
+                >
                     Welcome to <span className="text-primary"><span className="text-2xl md:text-3xl lg:text-5xl">D</span></span>evis<span className="text-2xl md:text-3xl lg:text-5xl text-primary">M</span>aster
-                </h1>
+                </motion.h1>
 
-                <p className='text-center text-foreground/80'>
-                    Do you need a quickly estimate devis for your projet ? <br/> let's jump to calculate it !
-                </p>
+                {/* Short description */}
+                <motion.p
+                    className='max-w-[100%] md:max-w-[80%] lg:max-w-[70%] text-center text-foreground/80'
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: .6, delay: 0.2, ease: "easeOut" }}
+                >
+                    Do you need a quickly estimate devis for your projet ? <br /> let's jump to calculate it !
+                </motion.p>
 
-                <CTA url='/new-projet'/>
-            </div>
+                {/* CTA button */}
+                <motion.div
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: .5, delay: 0.3, ease: "easeOut" }}
+                >
+                    <CTA url='/new-projet' />
+                </motion.div>
+            </section>
         </>
     )
 }

f82cf386b8dd966d67a003ee272af8ecc10b67ad
01Barthez <kenwoubarthez@gmail.com>
2025-02-20 14:36:31 +0100

    fix: Update Detail Project

--- src/pages/projet/DetailProjet.tsx
+++ src/pages/projet/DetailProjet.tsx
@@ -1,3 +1,4 @@
+import { BiCalendar } from "react-icons/bi";
 "use client"
 import React, { useEffect } from 'react'
 import { AiOutlineArrowRight } from "react-icons/ai";
@@ -20,7 +21,10 @@ import { useProjectStore } from "@/stores/projet.store";
 import type { IProject } from '@/interface/interface';
 import { phasesSchema } from '@/core/schema/schema';
 import { motion } from 'framer-motion';
-
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+import { Calendar } from '@/components/ui/calendar';
+import { format } from "date-fns"
 
 const DetailProjet: React.FC = () => {
     const navigate = useNavigate();
@@ -73,7 +77,7 @@ const DetailProjet: React.FC = () => {
         <section className="container space-y-10 py-10 md:py-16 lg:py-20 ">
             <div className="flex flex-col gap-2">
                 <h1 className='text-2xl md:text-3xl lg:text-4xl font-bold'>
-                    <span className="text-primary">D</span>etail of &nbsp;<span className="text-primary">P</span>rojet <br />
+                    <span className="text-primary">D</span>etail of &nbsp;<span className="text-primary">P</span>rojet Phases <br />
                     <span className="text-xl md:text-2xl lg:text-3xl">{project?.name}</span>
                 </h1>
 
@@ -89,58 +93,162 @@ const DetailProjet: React.FC = () => {
                     className="space-y-8 md:space-y-10 lg:space-y-16 w-full md:w-[80%] lg:w-[55%]"
                 >
                     <div className="space-y-4">
-                        <FormField
-                            control={form.control}
-                            name="name"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-base">
-                                        Name
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            placeholder="Name of projet"
-                                            autoComplete='off'
-                                            required
-                                            minLength={3}
-                                            maxLength={150}
-                                            className="border-foreground"
-                                            {...field}
-                                        />
-                                    </FormControl>
+                        <div className="flex items-center gap-1">
+                            <div className="yin-yang hidden md:block">
+                            </div>
+                            <h2 className="text-sm-title">
+                                Phase 1
+                            </h2>
+                        </div>
 
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
+                        <div className="space-y-2.5">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="text-base">
+                                            Name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Name of phase"
+                                                autoComplete='off'
+                                                required
+                                                minLength={3}
+                                                maxLength={150}
+                                                className="border-foreground"
+                                                {...field}
+                                            />
+                                        </FormControl>
 
-                        <FormField
-                            control={form.control}
-                            name="description"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-base">
-                                        Description
-                                    </FormLabel>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
 
-                                    <FormControl>
-                                        <Textarea
-                                            placeholder="Description of the projet"
-                                            autoComplete='off'
-                                            required
-                                            minLength={3}
-                                            maxLength={350}
-                                            className="resize-none border-foreground min-h-20"
-                                            {...field}
-                                        />
-                                    </FormControl>
+                            <FormField
+                                control={form.control}
+                                name="description"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="text-base">
+                                            Description
+                                        </FormLabel>
 
-                                    <FormMessage className="absolute animate-incommingErrorMSG" />
-                                </FormItem>
-                            )}
-                        />
+                                        <FormControl>
+                                            <Textarea
+                                                placeholder="Description of the phase"
+                                                autoComplete='off'
+                                                required
+                                                minLength={3}
+                                                maxLength={350}
+                                                className="resize-none border-foreground min-h-20"
+                                                {...field}
+                                            />
+                                        </FormControl>
+
+                                        <FormMessage className="absolute animate-incommingErrorMSG" />
+                                    </FormItem>
+                                )}
+                            />
+
+                            <div className="flex items-center gap-4 flex-col md:flex-row w-full">
+                                <FormField
+                                    control={form.control}
+                                    name="startDate"
+                                    render={({ field }) => (
+                                        <FormItem className="flex flex-col w-full">
+                                            <FormLabel className="text-base">
+                                                Start date of the phase
+                                            </FormLabel>
+
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <FormControl>
+                                                        <Button
+                                                            variant={"outline"}
+                                                            className={cn(
+                                                                "border-foreground w-full pl-3 text-left font-normal",
+                                                                !field.value && "text-muted-foreground"
+                                                            )}
+                                                        >
+                                                            {field.value ? (
+                                                                format(field.value, "PPP")
+                                                            ) : (
+                                                                <span>Pick a date</span>
+                                                            )}
+                                                            <BiCalendar className="ml-auto h-4 w-4 opacity-50" />
+                                                        </Button>
+                                                    </FormControl>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="w-auto p-0" align="start">
+                                                    <Calendar
+                                                        mode="single"
+                                                        selected={field.value}
+                                                        onSelect={field.onChange}
+                                                        disabled={(date) =>
+                                                            date < new Date()
+                                                        }
+                                                        initialFocus
+                                                    />
+                                                </PopoverContent>
+                                            </Popover>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+
+                                <FormField
+                                    control={form.control}
+                                    name="endDate"
+                                    render={({ field }) => (
+                                        <FormItem className="flex flex-col w-full">
+                                            <FormLabel className="text-base">
+                                                End date of the phase
+                                            </FormLabel>
+
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <FormControl>
+                                                        <Button
+                                                            variant={"outline"}
+                                                            className={cn(
+                                                                "border-foreground w-full pl-3 text-left font-normal",
+                                                                !field.value && "text-muted-foreground"
+                                                            )}
+                                                        >
+                                                            {field.value ? (
+                                                                format(field.value, "PPP")
+                                                            ) : (
+                                                                <span>Pick a date</span>
+                                                            )}
+                                                            <BiCalendar className="ml-auto h-4 w-4 opacity-50" />
+                                                        </Button>
+                                                    </FormControl>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="w-auto p-0" align="start">
+                                                    <Calendar
+                                                        mode="single"
+                                                        selected={field.value}
+                                                        onSelect={field.onChange}
+                                                        disabled={(date) => {
+                                                            const startDate = form.getValues("startDate");
+                                                            return !startDate || date < new Date(startDate);
+                                                        }}
+                                                        initialFocus
+                                                    />
+                                                </PopoverContent>
+                                            </Popover>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                        </div>
                     </div>
 
+                    {/* Navigations Buttons */}
                     <div className="flex items-center gap-3 md:gap-4 lg:gap-8">
                         <motion.div
                             initial={{ opacity: 0, scale: 0 }}
@@ -150,7 +258,8 @@ const DetailProjet: React.FC = () => {
                             <Button
                                 variant={'outline'}
                                 className='px-2 md:px-2 lg:px-3 border-foreground/30 text-foreground/70'
-                                onClick={ backOnNewProjet }
+                                onClick={backOnNewProjet}
+                                type='button'
                             >
                                 <span>Previous</span>
                             </Button>
